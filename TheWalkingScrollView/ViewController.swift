fbc3fcb86b2533f9658a5eaaa3221409593be345
Brian Bresen <dbumblebee@mac.com>
2016-11-10 11:36:46 -0600

    working, iPhone 6 screen size only

--- TheWalkingScrollView/ViewController.swift
+++ TheWalkingScrollView/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 370
+    let HEIGHT: CGFloat = 400
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 1...5 {
+            let img = UIImage(named: "\(x)")
+            let imgView = UIImageView(image: img)
+            imgView.contentMode = UIViewContentMode(rawValue: 1)!
+        
+            scrollView.addSubview(imgView)
+  
+            imgView.frame = CGRect(x: -WIDTH + (WIDTH * CGFloat(x)), y: 277, width: WIDTH, height: HEIGHT)
+        }
+        
+        scrollView.contentSize = CGSize(width: WIDTH * 5, height: scrollView.frame.size.height)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
